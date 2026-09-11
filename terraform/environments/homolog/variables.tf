807267212a670dc0ab53fecca3e1e566bbfa1344
amanda <amandacdesousa@outlook.com>
2026-09-11 00:40:00 -0300

    correcao versao postgres

--- terraform/environments/homolog/variables.tf
+++ terraform/environments/homolog/variables.tf
@@ -62,7 +62,7 @@ variable "db_password" {
 variable "engine_version" {
   description = "Versao do PostgreSQL."
   type        = string
-  default     = "16.3"
+  default     = "16.1"
 }
 
 variable "instance_class" {
